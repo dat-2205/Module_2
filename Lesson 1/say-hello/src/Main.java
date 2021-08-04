f30c759a6c2984db24a0e906985b6501ca4aa9b9
= <=>
2021-08-04 14:57:40 +0700

    MD2 bai1

---
+++ Lesson 1/say-hello/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+
+        System.out.println("Your Name: ");
+        name = sc.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
