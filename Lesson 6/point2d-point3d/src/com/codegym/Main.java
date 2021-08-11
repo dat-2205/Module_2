8d5bbf8d885f804caf441fd9442f57eae5522df3
= <=>
2021-08-11 15:07:31 +0700

    MD2 bai 6

---
+++ Lesson 6/point2d-point3d/src/com/codegym/Main.java
@@ -0,0 +1,16 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point2D a = new Point2D();
+        System.out.println(a);
+        Point2D b = new Point2D(3,8);
+        System.out.println(b);
+
+        Point3D x = new Point3D();
+        System.out.println(x);
+        Point3D y = new Point3D(2,2,5);
+        System.out.println(y);
+    }
+}
