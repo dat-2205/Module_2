c043f5db095193163c7de8958ebe342f01e2761d
= <=>
2021-08-18 15:48:23 +0700

    MD 2 bai 10

--- Lesson 10/my-list/src/MyList.java
+++ Lesson 10/my-list/src/MyList.java
@@ -1,2 +1,105 @@
-package PACKAGE_NAME;public class MyList {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+    public MyList(int capacity) {
+        elements = new Object[capacity];
+    }
+
+    public Object[] getElements() {
+        return  this.elements;
+    }
+    public int size() {
+        return this.size;
+    }
+    public boolean add(E e) {
+        ensureCapacity();
+        elements[size] = e;
+        size++;
+        return true;
+    }
+    public void add(int index, E e) {
+        ensureCapacity();
+        Object[] newArray = new Object[elements.length];
+        checkIndex(index);
+        for(int i=0;i<size+1;i++) {
+            if(i<index) {
+                newArray[i] = elements[i];
+            }else if(i==index) {
+                newArray[i] = e;
+            }else if(i>index) {
+                newArray[i] = elements[i-1];
+            }
+        }
+        elements = newArray;
+        size++;
+    }
+    public E remove(int index) {
+        Object[] newArray = new Object[elements.length];
+        checkIndex(index);
+        for(int i=0;i<size;i++) {
+            if(i<index) {
+                newArray[i] = elements[i];
+            }else if(i>index) {
+                newArray[i-1] = elements[i];
+            }
+        }
+        Object deleteElement = elements[index];
+        elements = newArray;
+        size--;
+        return (E) deleteElement;
+    }
+    public void ensureCapacity() {
+        if(size==elements.length) {
+            int newLength = elements.length*2;
+            elements = Arrays.copyOf(elements,newLength);
+        }
+    }
+    public MyList<E> clone() {
+        MyList<E> newList = new MyList<>();
+        newList.elements = this.elements;
+        newList.size = this.size;
+        return newList;
+    }
+    public boolean contains(E o) {
+        boolean isContain = false;
+        for(Object i : elements) {
+            if((E) i == o) {
+                isContain = true;
+                break;
+            }
+        }
+        return isContain;
+    }
+    public int indexOf(E o) {
+        int index = -1;
+        for(int i=0;i<size;i++) {
+            if((E) elements[i] == o) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+    public E get(int index) {
+        checkIndex(index);
+        return (E) elements[index];
+    }
+
+    private void checkIndex(int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index Wrong");
+        }
+    }
+    public void clear() {
+        elements = new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
 }
--- Lesson 10/my-list/src/MyListTest.java
+++ Lesson 10/my-list/src/MyListTest.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class MyListTest {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> list = new MyList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        list.add(2,13);
+
+        for (Object i: list.getElements()) {
+            System.out.println(i);
+        }
+        System.out.println("============================");
+        MyList<String> name = new MyList<>();
+        name.add("Huy");
+        name.add("NA");
+        name.add("Duc");
+        name.add("Dat");
+        for (Object i: name.getElements()) {
+            System.out.println(i);
+        }
+        System.out.println("================");
+        System.out.println(name.contains("Mia"));
+    }
 }
